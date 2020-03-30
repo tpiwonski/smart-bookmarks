54543c3eccd56991b15cc3842632b2f7bf27e5ae
Tomasz Piwoński <tpiwonski@gmail.com>
2020-03-30 22:28:06 +0200

    Add reverse with query string

---
+++ smart-bookmarks-django/smart_bookmarks/ui/utils.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from typing import List, Dict
+
+from django.urls import reverse
+from django.utils.http import urlencode
+
+
+def reverse_querystring(
+    view, urlconf=None, args=None, kwargs=None, current_app=None, query_kwargs=None
+):
+    """Custom reverse to handle query strings.
+    Usage:
+        reverse('app.views.my_view', kwargs={'pk': 123}, query_kwargs={'search', 'Bob'})
+    """
+    base_url = reverse(
+        view, urlconf=urlconf, args=args, kwargs=kwargs, current_app=current_app
+    )
+    if query_kwargs:
+        return "{}?{}".format(base_url, urlencode(query_kwargs))
+    return base_url
+
+
+@dataclass
+class ViewInfo:
+    viewname: str
+    args: List[any] = None
+    kwargs: Dict[str, any] = None
+    query: Dict[str, any] = None
